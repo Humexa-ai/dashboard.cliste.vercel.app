836478a2603caf3702cf41db4915dd2c6e50ecec
otooleai <hello@otoole.digital>
2025-10-31 22:52:13 +0000

    Rename Products/Services to Catalog

--- app/page.tsx
+++ app/page.tsx
@@ -53,8 +53,8 @@ export default function SidebarDemo() {
       ),
     },
     {
-      label: "Products/Services",
-      href: "/products",
+      label: "Catalog",
+      href: "/catalog",
       icon: (
         <ShoppingBag className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
